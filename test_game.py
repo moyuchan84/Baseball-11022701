3aff6689b3db5147d7cf220852a9ce2b6a1c1898
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 14:08:57 +0900

    [refactor]p38

--- test_game.py
+++ test_game.py
@@ -14,9 +14,10 @@ def assert_ilegal_argument(game, guessNumber):
         pass
 
 
-def test_exception_when_input_is_none(game):
+def test_exception_when_invalid_test(game):
     assert_ilegal_argument(game,None)
-
-
-def test_exception_when_input_is_unmatched(game):
     assert_ilegal_argument(game, "12")
+    assert_ilegal_argument(game, "1234")
+
+
+
