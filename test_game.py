c6ef17d579b6b54e492c9f96400c07ae49acb234
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 14:01:51 +0900

    [feature]p31

--- test_game.py
+++ test_game.py
@@ -10,3 +10,8 @@ def test_exception_when_input_is_none(game):
 
     with pytest.raises(TypeError):
         game.guess(None)
+
+
+def test_exception_when_input_is_unmatched(game):
+    with pytest.raises(TypeError):
+        game.guess("12")
