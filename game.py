c7a38f6e235b4af9880d5e50bf5f2a2ddf7c7e2b
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 14:17:01 +0900

    [refactor]p47(guessNum->guess_num)

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 class Game:
-    def guess(self, guessNumber):
-        self._assert_illegal_value(guessNumber)
+    def guess(self, guess_number):
+        self._assert_illegal_value(guess_number)
 
-    def _assert_illegal_value(self, guessNumber):
-        if guessNumber is None:
+    def _assert_illegal_value(self, guess_number):
+        if guess_number is None:
             raise TypeError('입력이 None입니다')
-        if len(guessNumber) != 3:
+        if len(guess_number) != 3:
             raise TypeError('입력은 3자리 문자열이여야합니다.')
-        if not guessNumber.isdigit():
+        if not guess_number.isdigit():
                 raise TypeError('모든 문자는 숫자로 구성되어야 합니다.')
-        if self.inDuplicatedNumber(guessNumber):
+        if self.inDuplicatedNumber(guess_number):
             raise TypeError('중복된 숫자가 존재합니다')
 
     def inDuplicatedNumber(self, guessNumber):
