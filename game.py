d03a70cf4d692794b843ca2bdaaa013f54de1b55
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 14:16:25 +0900

    [refactor]p47

--- game.py
+++ game.py
@@ -7,8 +7,7 @@ class Game:
             raise TypeError('입력이 None입니다')
         if len(guessNumber) != 3:
             raise TypeError('입력은 3자리 문자열이여야합니다.')
-        for number in guessNumber:
-            if not ord('0') <= ord(number) <= ord('9'):
+        if not guessNumber.isdigit():
                 raise TypeError('모든 문자는 숫자로 구성되어야 합니다.')
         if self.inDuplicatedNumber(guessNumber):
             raise TypeError('중복된 숫자가 존재합니다')
