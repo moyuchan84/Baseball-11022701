2dbc813a7e22b8711373ea1172cb3814b5e91210
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 14:12:21 +0900

    [refactor]p42

--- test_game.py
+++ test_game.py
@@ -19,6 +19,7 @@ def test_exception_when_invalid_test(game):
     assert_ilegal_argument(game, "12")
     assert_ilegal_argument(game, "1234")
     assert_ilegal_argument(game, "12s")
+    assert_ilegal_argument(game, "121")
 
 
 
