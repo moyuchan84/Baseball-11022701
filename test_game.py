23a6e225bc907de2cdfd827c672bc32d443081c9
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 14:32:03 +0900

    [feature]p61

--- test_game.py
+++ test_game.py
@@ -38,7 +38,11 @@ def test_return_solved_result_if_unmatched_number(game):
     game.question = '123'
     result:GameResult = game.guess('456')
 
+    solved = False
+    strikes = 0
+    balls = 0
+
     assert result is not None
-    assert result.solved == False
-    assert result.strikes == 0
-    assert result.balls == 0
+    assert result.solved == solved
+    assert result.strikes == strikes
+    assert result.balls == balls
